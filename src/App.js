3dd23541ba7a279b61f1b5eaaae4bb08f438f809
Sarah Aldous <sealdous@gmail.com>
2019-09-26 14:15:45 -0700

    just barely starting this React project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Drumkit from '././projects/01-drum-kit/01-drum-kit.js'
+import './style.css'
+
+function App(){
+    return (
+        <div>Snowdrops</div>
+        // <Drumkit />
+    )
+}
+export default App
---
+++ src/projects/01-drum-kit/01-drum-kit.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './01-drum-kit.css'
+// import boom from './sounds/boom.wav'
+// import clap from './sounds/clap.wav'
+// import hihat from './sounds/hihat.wav'
+// import kick from './sounds/kick.wav'
+// import openhat from './sounds/openhat.wav'
+// import ride from './sounds/ride.wav'
+// import snare from './sounds/snare.wav'
+// import tink from './sounds/tink.wav'
+// import tom from './sounds/tom.wav'
+// import clapIcon from './clapping.png'
+// import drumkitIcon from './drums.png'
+// import drumsticksIcon from './drumsticks.png'
+
+function Drumkit(){
+    return (
+        <div>
+            <h1>The Drum Kit exercise will go here.</h1>
+        </div>
+    )
+}
+export default Drumkit
